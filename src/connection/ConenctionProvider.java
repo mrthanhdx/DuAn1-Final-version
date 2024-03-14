e916f31d83031025cccbec46cf8f6d2a7b1812e2
pongpz <129265131+pongpz@users.noreply.github.com>
2024-03-14 21:26:24 +0700

    Update ConenctionProvider.java

--- src/connection/ConenctionProvider.java
+++ src/connection/ConenctionProvider.java
@@ -6,10 +6,9 @@ public class ConenctionProvider {
     // Connection
     private static Connection conn;
     private static String DB_SERVERNAME = "localhost";
-    private static String DB_DATABASENAME = "Duan1";
+    private static String DB_DATABASENAME = "DuAn1";
     private static String DB_USERNAME = "sa";
     private static String DB_PASSWORD = "12345678";
-    //hi sa
    
     public static Connection getConnection () {
         if (conn!=null) {
